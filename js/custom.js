cd138e4f6faa9a480c0faf0d8713686839eb35ae
Olga Duka <olgaduk@mail.ru>
2018-03-02 22:43:23 +0600

    Скроллбар - версия 1

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,62 @@
 'use strict';
-(function ($) {
-// Preloader - анимация, пока ждем загрузку страницы (шрифты, скрипты и т.д.)
-  $(window).load(function () {
-    $('.status').fadeOut();
-    $('.preloader').delay(300).fadeOut('slow');
+(function () {
+  // ----------------------------------------------
+  // Меню
+  // ----------------------------------------------
+  var header = document.querySelector('.header');
+  var toggle = header.querySelector('.header__toggle');
+  var menu = header.querySelector('.menu');
+  var title = header.querySelector('.header__title');
+  var intro = header.querySelector('.header__intro');
+  // Закрываем меню, если JS работает
+  menu.classList.remove('menu--nojs');
+  menu.classList.add('menu--close');
+  toggle.classList.remove('header__toggle--cross');
+  title.classList.remove('header__title--hidden');
+  intro.classList.remove('header__intro--hidden');
+  header.style.height = '280px';
+  // Переключаем состояние меню по кнопке и меняем высоту хедера
+  toggle.addEventListener('click', function () {
+    menu.classList.toggle('menu--close');
+    toggle.classList.toggle('header__toggle--cross');
+    title.classList.toggle('header__title--hidden');
+    intro.classList.toggle('header__intro--hidden');
+    header.style.height = (header.style.height === '280px') ? '480px' : '280px';
   });
 
-  $(document).ready(function () {
-    var toggle = document.querySelector('.header__toggle');
-    var menu = document.querySelector('.menu');
+  // ----------------------------------------------
+  // Слайдер
+  // ----------------------------------------------
+  var slides = document.querySelectorAll('.ideas__slide');
+  var sliderBtns = document.querySelectorAll('.ideas__btn');
+  var slideCurrentNum = 0;
+  sliderBtns[0].classList.add('ideas__btn--hidden');
+  slides[0].classList.add('ideas__slide--active');
+  // Функции для обработки событий на слайдере
+  var onClickButtonBack = function () {
+    if (slideCurrentNum === (slides.length - 1)) {
+      sliderBtns[1].classList.remove('ideas__btn--hidden');
+    }
+    slides[slideCurrentNum].classList.remove('ideas__slide--active');
+    slideCurrentNum--;
+    slides[slideCurrentNum].classList.add('ideas__slide--active');
+    if (slideCurrentNum === 0) {
+      sliderBtns[0].classList.add('ideas__btn--hidden');
+    }
+  };
+  var onClickButtonForward = function () {
+    if (slideCurrentNum === 0) {
+      sliderBtns[0].classList.remove('ideas__btn--hidden');
+    }
+    slides[slideCurrentNum].classList.remove('ideas__slide--active');
+    slideCurrentNum++;
+    slides[slideCurrentNum].classList.add('ideas__slide--active');
+    if (slideCurrentNum === (slides.length - 1)) {
+      sliderBtns[1].classList.add('ideas__btn--hidden');
+    }
+  };
+  // Установка обработчиков событий для слайдера
+  sliderBtns[0].addEventListener('click', onClickButtonBack);
+  sliderBtns[1].addEventListener('click', onClickButtonForward);
 
-    // ----------------------------------------------
-    // "Мягкий" скроллинг при нажатии на пункт меню или кнопки перехода
-    // ----------------------------------------------
-
-    $(document).on('scroll', onScroll);
-
-    $('a[href*=#]').on('click', function (e) {
-      var anchor = $(this);
-      $(document).off('scroll');
-
-      $('html, body').stop().animate({'scrollTop': $(anchor.attr('href')).offset().top}, 1000, function () {
-        $(document).on('scroll', onScroll);
-      });
-    });
-
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
-        $('.scroll--up').fadeIn();
-      } else {
-        $('.scroll--up').fadeOut();
-      }
-    });
-
-    // ----------------------------------------------
-    // Меню
-    // ----------------------------------------------
-
-    // Закрываем меню, если JS работает
-    menu.classList.remove('menu--nojs');
-    menu.classList.add('menu--close');
-    toggle.classList.remove('header__toggle--cross');
-    // Переключаем состояние меню по кнопке
-    toggle.addEventListener('click', function () {
-      menu.classList.toggle('menu--close');
-      toggle.classList.toggle('header__toggle--cross');
-    });
-
-    // Закрываем меню, если оно выпадающее, переключаем вид кнопки
-    var onMenuClick = function () {
-      menu.classList.add('menu--close');
-      toggle.classList.toggle('header__toggle--cross');
-    };
-
-    // Добавляем событие клика на меню
-    menu.addEventListener('click', onMenuClick);
-
-  });
-})(jQuery);
+})();
