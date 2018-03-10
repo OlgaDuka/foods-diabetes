6d619e22fb8f90f857d2dbc274e2f9257356f38e
Olga Duka <olgaduk@mail.ru>
2018-03-10 12:43:57 +0600

    Last version

--- js/myfirst.js
+++ js/myfirst.js
@@ -1,28 +1,41 @@
 'use strict';
 $(function () {
-// Меню
-  var header = document.querySelector('.header');
-  var toggle = header.querySelector('.header__toggle');
-  var menu = header.querySelector('.menu');
-  var title = header.querySelector('.header__title');
-  var intro = header.querySelector('.header__intro');
-
-  menu.classList.remove('menu--close');
-  toggle.classList.add('header__toggle--cross');
-  title.classList.add('header__title--hidden');
-  intro.classList.add('header__intro--hidden');
-  $('.header__menu').slideToggle();
-  toggle.classList.toggle('header__toggle--cross');
-  title.classList.toggle('header__title--hidden');
-  intro.classList.toggle('header__intro--hidden');
-
+  // ---------------------------------------
+  // Меню (Стандартные функции JQuery)
+  // -----------------------------------------
+  var desktopWidth = 1170;
+  $('.header__menu').removeClass('menu--nojs');
+  if ($(window).width() < desktopWidth) {
+    $('.header__toggle').removeClass('header__toggle--cross');
+    $('.header__menu').addClass('menu--close');
+    $('.header__menu').slideUp('slow');
+    $('.header__title').removeClass('header__title--hidden');
+    $('.header__intro').removeClass('header__intro--hidden');
+  }
   $('.header__toggle').click(function () {
-    $('.header__menu').slideToggle();
-    toggle.classList.toggle('header__toggle--cross');
-    title.classList.toggle('header__title--hidden');
-    intro.classList.toggle('header__intro--hidden');
+    $('.header__menu').slideToggle('slow');
+    $('.header__title').slideToggle('fast');
+    $('.header__intro').slideToggle('fast');
+    $('.header__toggle').toggleClass('header__toggle--cross');
   });
 
+  // Перезагрузка страницы, если изменение идет между мобильной и десктопной версиями
+  var oldWidth = $(window).width();
+  $(window).resize(function () {
+    var newWith = $(window).width();
+    if ((oldWidth < desktopWidth) && (newWith >= desktopWidth)) {
+      location.reload();
+      $('.header__menu').removeClass('menu--nojs');
+      $('.header__menu').removeClass('menu--close');
+      return;
+    } else if ((oldWidth >= desktopWidth) && (newWith < desktopWidth)) {
+      location.reload();
+    }
+  });
+
+  // -------------------------------------------------------------------
+  // Оживление слайдеров с помощью плагина Swiper (работает без JQuery)
+  // --------------------------------------------------------------------
   // Слайдер Партнеры
   var swiper1 = new Swiper('.swiper-container--1', {
     slidesPerView: 'auto',
@@ -55,20 +68,22 @@ $(function () {
     },
   });
 
+  // -------------------------------------------------------------
+  // Стандартные функции JQuery для скроллинга
+  // -------------------------------------------------------------
+  // Плавный скроллинг по сайту до якорей при нажатии на пункты меню
   $('#menu').on('click', 'a', function (event) {
-    // отменяем стандартную обработку нажатия по ссылке
     event.preventDefault();
-    // забираем идентификатор бока с атрибута href
+    // забираем идентификатор блока с атрибута href
     var id = $(this).attr('href');
     // узнаем высоту от начала страницы до блока на который ссылается якорь
     var top = $(id).offset().top;
     // анимируем переход на расстояние - top за 1500 мс
     $('body,html').animate({scrollTop: top}, 1500);
   });
-
+  // Появление и исчезновение кнопки "наверх" на первом экране
   var limit = $(window).height() / 3;
   var $backToTop = $('#scroll-up');
-
   $(window).scroll(function () {
     if ($(this).scrollTop() > limit) {
       $backToTop.fadeIn();
@@ -76,12 +91,14 @@ $(function () {
       $backToTop.fadeOut();
     }
   });
-
+  // Мягкий скролл к началу сайта
   $backToTop.click(function () {
     $('body,html').animate({scrollTop: 0}, 1500);
     return false;
   });
 
+  // Скроллинг на десктопной версии блока идей потребителей
+  // с помощью плагина Scroll-pane (JQuery)
   $('.scroll-pane').jScrollPane({
     showArrows: false,
     enableKeyboardNavigation: false,
